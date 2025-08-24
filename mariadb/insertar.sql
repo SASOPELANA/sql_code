7308f93c5ca9e4059e8907fe64379b4c60f9c86f
SergioDeb <sopekof@gmail.com>
2025-08-23 23:02:32 -0300

    Consultas - SELECT

--- mariadb/insertar.sql
+++ mariadb/insertar.sql
@@ -1,16 +1,12 @@
-INSERT INTO estudiantes
-  (
-  nombre,
-  apellido,
-  email
-  )
-VALUES(
-    'Daniel',
-    'Coso',
-    'daniel@gmail.com'
-),
-  (
-    'Gisel',
-    'Petra',
-    'gisel@gmail.com'
-);
+INSERT INTO estudiantes(nombre,apellido,edad,email) VALUES
+('Andres','Guzman',20,'3aZdH2@example.com'),
+('Daniel','Pro',18,'daniel1@pro.com'),
+('Sergio','Guzman',30,'sergio2@pro12.com'),
+('Gustavo','Tilix',20,'gustavo2@example.com'),
+('Maria','Angeles',20,'maria2@test.com'),
+('Nicol','Perez',22,'nicol@test.com'),
+('Eduardo','Luna',22,'eduardo@test.com'),
+('Juan','Perez',28,'juan12@example.com'),
+('Jorge','Termo',32,'jorge@example.com'),
+('Soledad','Gusty',22,'soledad@example.com'),
+('Belen','Gimenez',19,'belen@example.com'); 
